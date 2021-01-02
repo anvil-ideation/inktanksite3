178d22c283e257e03ac38c88a26e831b272b700c
anvil-ideation <landon@anvilideation.com>
2021-01-02 09:04:34 -0700

    Home Featured Books Initial Setup

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { BOOKS } from '../shared/books';
+
+function RenderItem({item}) {
+
+    if (item) {        
+        return (
+            <Card
+                featuredTitle={item.title}
+                image={{ uri: item.image }}
+                containerStyle={{
+                    width: 100,
+                }}
+            >
+                <Text style={{
+                        alignSelf: 'center',
+                        fontStyle: 'italic',
+                        fontWeight: 'bold',
+                        fontSize: 8,
+                    }}>
+                    by {item.author}
+                </Text>
+                <Text style={{
+                        alignSelf: 'center',
+                        fontStyle: 'italic',
+                        fontSize: 8,
+                    }}>
+                    {item.category} Book
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: BOOKS,
+        };
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,9 +50,30 @@ class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <Text style={{
+                        marginTop: 5,
+                        alignSelf: 'center',
+                        fontStyle: 'italic',
+                        fontSize: 14,
+                    }}>
+                    Featured Books This Month:
+                </Text>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                }}>
+                    <RenderItem
+                        item={this.state.books.filter(book => book.featured)[0]}
+                    />
+                    <RenderItem 
+                        item={this.state.books.filter(book => book.featured)[1]}
+                    />
+                    <RenderItem 
+                        item={this.state.books.filter(book => book.featured)[2]}
+                    />
+                </View>
+            </ScrollView>
         );
     }
 }
